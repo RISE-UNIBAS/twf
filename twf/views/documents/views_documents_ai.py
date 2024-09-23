11a0ccc437cef2bb06c331ccf34adac30a565eb1
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2024-09-23 22:48:58 +0200

    Worked on document batches

--- twf/views/documents/views_documents_ai.py
+++ twf/views/documents/views_documents_ai.py
@@ -9,13 +9,14 @@ from django.views.generic import FormView
 
 from twf.forms.project_forms import AIQueryDatabaseForm, BatchOpenAIForm
 from twf.clients.simple_ai_clients import AiApiClient
+from twf.views.documents.views_documents import TWFDocumentView
 from twf.views.project.views_project import TWFProjectView
 from twf.tasks.openai_tasks import ask_chatgpt_task
 
 logger = logging.getLogger(__name__)
 
 
-class TWFDocumentOpenAIBatchView(FormView, TWFProjectView):
+class TWFDocumentOpenAIBatchView(FormView, TWFDocumentView):
     """Ask ChatGPT."""
     template_name = 'twf/documents/batches/openai_batch_query.html'
     page_title = 'Geonames Batch'
@@ -28,7 +29,7 @@ class TWFDocumentOpenAIBatchView(FormView, TWFProjectView):
         return kwargs
 
 
-class TWFDocumentGeminiBatchView(FormView, TWFProjectView):
+class TWFDocumentGeminiBatchView(FormView, TWFDocumentView):
     """Ask Gemini."""
     template_name = 'twf/documents/batches/gemini_batch_query.html'
     page_title = 'Gemini Batch'
@@ -41,7 +42,7 @@ class TWFDocumentGeminiBatchView(FormView, TWFProjectView):
         return kwargs
 
 
-class TWFDocumentClaudeBatchView(FormView, TWFProjectView):
+class TWFDocumentClaudeBatchView(FormView, TWFDocumentView):
     """Ask Claude."""
     template_name = 'twf/documents/batches/claude_batch_query.html'
     page_title = 'Claude Batch'
