5e56c6cc11b99f61904290b7d1c4cc1faa497ca2
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2024-09-29 14:28:45 +0200

    Moved google sheet client to appropriate folder and renamed it.

--- twf/clients/google_sheets_client.py
+++ twf/clients/google_sheets_client.py
@@ -3,7 +3,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 
-class MetadataManager:
+class GoogleSheetsClient:
     """Manages metadata from Google Sheets."""
 
     service = None
@@ -11,23 +11,23 @@ class MetadataManager:
     @staticmethod
     def initialize_service(service_account_json):
         """Initializes the Google Sheets service object."""
-        if MetadataManager.service is None:
+        if GoogleSheetsClient.service is None:
             credentials = Credentials.from_service_account_file(
                                                 service_account_json,
                                                 scopes=['https://www.googleapis.com/auth/spreadsheets.readonly'])
 
-            MetadataManager.service = build('sheets', 'v4', credentials=credentials)
+            GoogleSheetsClient.service = build('sheets', 'v4', credentials=credentials)
 
     @staticmethod
     def get_service(service_account_json):
         """Returns a Google Sheets service object."""
-        MetadataManager.initialize_service(service_account_json)
-        return MetadataManager.service
+        GoogleSheetsClient.initialize_service(service_account_json)
+        return GoogleSheetsClient.service
 
     @staticmethod
     def get_title_row(service_account_json, spreadsheet_id, range_name):
         """Returns the title row of a Google Sheet as a list."""
-        values = MetadataManager.get_data_from_spreadsheet(service_account_json, spreadsheet_id, range_name)
+        values = GoogleSheetsClient.get_data_from_spreadsheet(service_account_json, spreadsheet_id, range_name)
         if values is not None:
             values = values[0]
         return values
@@ -35,7 +35,8 @@ class MetadataManager:
     @staticmethod
     def get_data_from_spreadsheet(service_account_json, spreadsheet_id, range_name):
         """Requests data from a Google Sheet and returns it as a list of lists."""
-        sheet = MetadataManager.get_service(service_account_json).spreadsheets()
+
+        sheet = GoogleSheetsClient.get_service(service_account_json).spreadsheets()
         result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
         values = result.get('values', [])
 
