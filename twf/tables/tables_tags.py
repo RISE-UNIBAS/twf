082d4b721ed733b202c64b50030bea265b628e0e
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2024-11-01 13:13:25 +0100

    Changed tag table to show correct normalized_entries for dates

--- twf/tables/tables_tags.py
+++ twf/tables/tables_tags.py
@@ -7,6 +7,9 @@ from twf.models import PageTag
 
 class TagTable(tables.Table):
     """Table for displaying tags."""
+
+    entry = tables.Column(accessor="dictionary_entry", verbose_name="Entry", empty_values=())
+
     options = tables.TemplateColumn(template_name='twf/tables/tag_table_options.html',
                                     verbose_name="Options",
                                     attrs={"td": {"width": "10%"}},
@@ -16,10 +19,23 @@ class TagTable(tables.Table):
         """Meta class for the TagTable."""
         model = PageTag
         template_name = "django_tables2/bootstrap.html"
-        fields = ("variation", "variation_type", "dictionary_entry")
+        fields = ("variation", "variation_type", "entry")
         attrs = {
             'class': 'table table-striped'
         }
+
+    def render_entry(self, value, record):
+        """Render entry field based on variation_type."""
+        date_types = self.get_date_types()
+        print(f"Variation Type: {record.variation_type}, Value: {value}")
+        if record.variation_type in date_types:
+            return record.date_variation_entry.edtf_of_normalized_variation or "-"
+        return record.dictionary_entry or "-"
+
+    def get_date_types(self):
+        """Retrieve or define the date types here."""
+        # Define or fetch your date types. Example:
+        return ["date", "date_type_2"]  # Replace with actual date types
 
 
 class TagDateTable(tables.Table):
